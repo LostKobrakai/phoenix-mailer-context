eb377e52325dfcdbd9badd1a9b4c1ccd23d04858
Benjamin Milde <benni@kobrakai.de>
2017-07-06 09:16:31 +0200

    Send email in page controller to test things out

--- lib/phx_mailer/web/controllers/page_controller.ex
+++ lib/phx_mailer/web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule PhxMailer.Web.PageController do
   use PhxMailer.Web, :controller
 
   def index(conn, _params) do
+    PhxMailer.Mailer.Mails.text_and_html_email_without_layouts
+    |> PhxMailer.Mailer.deliver_now
+    
     render conn, "index.html"
   end
 end
